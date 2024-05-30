99331fb5d1e257d76f912b4374844a919cf13596
Afsana Mammadova 16063 <amammadova16063@ada.edu.az>
2024-05-31 00:11:43 +0400

    day05 - coding tasks

---
+++ day05/fizzbuzz.py
@@ -0,0 +1,16 @@
+# Write your code here 👇
+for n in range(0, 100):
+  n += 1
+  if n % 5 == 0 and n % 3 == 0:
+    print("FizzBuzz")
+  elif n % 3 == 0:
+    print("Fizz")
+  elif n % 5 == 0:
+    print("Buzz")
+  else:
+    print(n)
+    
+
+
+
+  
