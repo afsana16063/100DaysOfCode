7dfe4063866ec02f3208552d923aac7d72b29871
Afsana Mammadova 16063 <amammadova16063@ada.edu.az>
2024-05-31 20:35:46 +0400

    day06 started

---
+++ day06/reeborgs_world.py
@@ -0,0 +1,27 @@
+# a task done in https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+    
+# def algorithm():
+#     move()
+#     turn_left()
+#     move()
+#     turn_around()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+# while not at_goal():
+#     algorithm()
+
+
